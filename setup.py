0a14b7b12d6d10d58f8a954ab983eb010b8b7d78
Daniel <debonzi@gmail.com>
2018-08-16 18:03:19 -0300

    New Release with working Pypi Description.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extras_require = {
 
 
 setup(name='gces',
-      version='0.0.2-alpha',
+      version='0.0.3-alpha',
       description='Google Cloud Event System.',
       long_description=long_description,
       long_description_content_type='text/markdown',
